cd569bcd2864e11aae951e48f39ad412b19e47b2
Magedsaif <magedsaif21@gmail.com>
2023-06-06 23:54:09 +0200

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-last_number = number % 10
-if (number < 0):
-	if (last_number > 5):
-		print(f"Last digit of {number:d} is {last_number:d} and is greater than 5")
-	elif (last_number < 5) and (last_number != 0):
-		print(f"Last digit of {number:d} is {last_number:d} less than 6 and not 0")
-	elif (last_number == 0):
-		print(f"Last digit of {number:d} is {last_number:d}")
-elif (number > 0):
-    if (last_number > 5):
-        print(f"Last digit of {number:d} is {last_number:d} and is greater than 5")
-    elif (last_number < 5) and (last_number != 0):
-        print(f"Last digit of {number:d} is {last_number:d} less than 6 and not 0")
-    elif (last_number == 0):
+
+if number < 0:
+    last_number = -(-number % 10)
+    if last_number > 5:
+        print(f"Last digit of {number:d} is {last_number:d} \
+and is greater than 5")
+    elif last_number < 5 and last_number != 0:
+        print(f"Last digit of {number:d} is {last_number:d} \
+less than 6 and not 0")
+    elif last_number == 0:
+        print(f"Last digit of {number:d} is {last_number:d}")
+elif number > 0:
+    last_number = number % 10
+    if last_number > 5:
+        print(f"Last digit of {number:d} is {last_number:d} \
+and is greater than 5")
+    elif last_number < 5 and last_number != 0:
+        print(f"Last digit of {number:d} is {last_number:d} \
+less than 6 and not 0")
+    elif last_number == 0:
         print(f"Last digit of {number:d} is {last_number:d}")
 else:
     print(f"Last digit of {number:d} is 0 and is 0")
