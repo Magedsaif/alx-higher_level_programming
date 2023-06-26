06f717754ca00ec195ab4b4ed816c4833fa53dd0
Magedsaif <magedsaif21@gmail.com>
2023-06-27 00:17:40 +0200

    task 0 edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,5 @@ def safe_print_list(my_list=[], x=0):
             except IndexError:
                 print()
                 return (elements_printed)
-        else:
-            print()
-            return (elements_printed)
+        print()
+        return (elements_printed)
