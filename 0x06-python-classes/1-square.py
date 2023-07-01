95821a1d1076aecf738da20fa1c1e5ed22f2880e
Magedsaif <magedsaif21@gmail.com>
2023-07-02 00:37:43 +0200

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+classes and objects tasks
+"""
+
+
+class Square:
+    """
+    square class
+    """
+    def __init__(self, size):
+        """
+        the constructor for Square class
+        Args:
+        size (int) : the size of the square
+        """
+        self.__size = size
