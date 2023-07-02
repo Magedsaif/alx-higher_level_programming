649a9fbe9e2c9b33dbf633b2eb2568c8a7cb4a78
Magedsaif <magedsaif21@gmail.com>
2023-07-02 23:39:17 +0200

    task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that prints a message."""
+
+def say_my_name(first_name, last_name=""):
+    """Create A function that prints a message.
+
+    Args:
+        first_name (str): name to be entered
+        last_name (str): name to be entered. Defaults to "".
+    """
