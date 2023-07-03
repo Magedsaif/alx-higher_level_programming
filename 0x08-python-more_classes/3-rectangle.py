872035caa95fcce09fc63ccb581061f9d20996a8
Magedsaif <magedsaif21@gmail.com>
2023-07-03 13:54:16 +0200

    edit task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -61,6 +61,8 @@ class Rectangle:
 
     def __str__(self):
         """Visualise the object representation."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
         rows = []
         for x in range(self.__height):
             row = "#" * self.__width
