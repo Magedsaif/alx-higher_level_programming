8084ea456a774244f2abf31f861fc9e5ff63c805
Magedsaif <magedsaif21@gmail.com>
2023-07-03 04:03:42 +0200

    add task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A function that prints a message."""
 
+
 def say_my_name(first_name, last_name=""):
     """Create A function that prints a message.
 
@@ -8,3 +9,8 @@ def say_my_name(first_name, last_name=""):
         first_name (str): name to be entered
         last_name (str): name to be entered. Defaults to "".
     """
+    if not isinstance(first_name, (str)):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, (str)):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
