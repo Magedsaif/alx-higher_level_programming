a0131d0ecc20622bf19ee1818b81a345fac3789e
Magedsaif <magedsaif21@gmail.com>
2023-07-04 22:10:10 +0200

    task 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Prints a text with 2 new lines after each of these characters:., ? and:."""
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines.
+
+    Args:
+        text (st): text given
+
+    Raises:
+        TypeError: text must be a string
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for character in ":.?":
+        text = text.replace(character, character + "\n\n")
+
+    list_lines = []
+    splitted_text = text.split('\n')
+    for line in splitted_text:
+        stripped_line = line.strip(' ')
+        list_lines.append(stripped_line)
+
+    revised_lines = "\n".join(list_lines)
+
+    print(revised_lines, end="")
