dcbf52f2dc5486f416236ad48898560411d1074e
Magedsaif <magedsaif21@gmail.com>
2023-07-11 00:30:27 +0200

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class MyList that inherits from list."""
+
+
+class MyList(list):
+    """Class MyList that inherits from list."""
+
+    def print_sorted(self):
+        """Print sorted list."""
+        print(sorted(self))
