5e9454a21385bb19fcfeff97b781070f1ac9ff61
Magedsaif <magedsaif21@gmail.com>
2023-07-11 00:42:14 +0200

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python Script."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if the object is an instance of.
+
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False.
+    """
+    return isinstance(obj, a_class)
