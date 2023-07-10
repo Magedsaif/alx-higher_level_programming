a643dce328b73f7166d05e0a273ff4805d5316f4
Magedsaif <magedsaif21@gmail.com>
2023-07-11 00:58:19 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python Script."""
+
+
+class BaseGeometry:
+    """BaseGeometry."""
+
+    def area(self):
+        """Area."""
+        raise Exception("area() is not implemented")
