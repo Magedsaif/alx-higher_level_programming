f5bb7e5e65face998a06c4f2aa2f6656b492e9e9
Magedsaif <magedsaif21@gmail.com>
2023-07-11 00:50:13 +0200

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Python Script."""
+
+
+def inherits_from(obj, a_class):
+    """Return True if the object.
+
+    is an instance of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+    """
+    if type (obj) == a_class:
+        return False
+    return issubclass(type(obj),a_class)
