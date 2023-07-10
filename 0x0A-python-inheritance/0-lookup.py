8b5e3a812c48739172512e35b5132f854c28d779
Magedsaif <magedsaif21@gmail.com>
2023-07-11 00:14:46 +0200

    task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that returns the list of attributes.
+
+and methods of an object.
+"""
+
+
+def lookup(obj):
+    """Return list of Att and methods.
+
+    Args:
+        obj (object): object
+    """
+    list = dir(obj)
+    return list
