309872ef4e1bb69abe5827c18855c4042e5cc462
Magedsaif <magedsaif21@gmail.com>
2023-07-11 18:12:15 +0200

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write file."""
+
+
+def write_file(filename="", text=""):
+    """Write file."""
+# Open the text file using "with" statement to ensure it is closed after use.
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return(f.write(text))
