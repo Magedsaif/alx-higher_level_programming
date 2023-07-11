ea6c261d6936fb67c9dc6d43e5d8ca0650bb14a5
Magedsaif <magedsaif21@gmail.com>
2023-07-11 21:34:42 +0200

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script."""
+
+
+import json
+import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+try:
+    jsonList = load_from_json_file("add_item.json")
+except:
+    jsonList = []
+
+for i in sys.argv[1:]:
+    jsonList.append(i)
+save_to_json_file(jsonList, "add_item.json")
