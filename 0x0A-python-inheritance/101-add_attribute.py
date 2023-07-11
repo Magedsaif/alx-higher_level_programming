3a21a41949c12c26e0c0c34bca762cee674a1d79
Magedsaif <magedsaif21@gmail.com>
2023-07-11 02:09:48 +0200

    task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python Script."""
+
+def add_attribute(obj, attribute, value):
+    """Add attribute to object if possible."""
+    if ('__dict__' in dir(obj)):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
