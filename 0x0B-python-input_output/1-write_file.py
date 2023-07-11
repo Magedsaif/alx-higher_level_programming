f11173178597fa03134b4b2de7e39c7eca1769c2
Magedsaif <magedsaif21@gmail.com>
2023-07-11 18:13:15 +0200

    edit task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """Write file."""
 # Open the text file using "with" statement to ensure it is closed after use.
     with open(filename, mode='w', encoding='utf-8') as f:
-        return(f.write(text))
+        return (f.write(text))
