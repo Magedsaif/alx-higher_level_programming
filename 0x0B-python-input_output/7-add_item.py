fa9118c2386b8a31a9a2336758c848e5c529caf4
Magedsaif <magedsaif21@gmail.com>
2023-07-12 23:30:56 +0200

    task 9

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-"""Python script."""
-
-
-import sys
+"""load_from_json_file function"""
 import json
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def save_to_json_file(my_obj, filename):
-    """Object to a text file, using a JSON representation."""
-    with open(filename, 'w', encoding="utf-8") as f:
-        # Use the `indent` parameter for pretty-printing.
-        return json.dump(my_obj, f)
+def main():
+    list_argv = []
+    for arg in sys.argv[1:]:
+        list_argv.append(arg)
+
+    name = 'add_item.json'
+
+    if not os.path.exists(name):
+        with open(name, 'w') as file:
+            file.write("[]")
+
+    existing_data = load_from_json_file(name)
+    existing_data.extend(list_argv)
+
+    save_to_json_file(existing_data, name)
+
+    load_from_json_file(name)
 
 
-def load_from_json_file(filename):
-    """Create an Object from a “JSON file”."""
-    with open(filename, 'r', encoding="utf-8") as f:
-        return json.load(f)
-
-
-def add_arguments_to_list(arguments):
-    try:
-        existing_list = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        existing_list = []
-
-    existing_list.extend(arguments)
-    save_to_json_file(existing_list, 'add_item.json')
-
-
-if __name__ == '__main__':
-    arguments = sys.argv[1:]
-    add_arguments_to_list(arguments)
+if __name__ == "__main__":
+    main()
