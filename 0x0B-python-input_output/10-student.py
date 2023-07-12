87688b1124f66897f455bfce9d0b2ac888808bb5
Magedsaif <magedsaif21@gmail.com>
2023-07-13 01:00:29 +0200

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""Python script."""
+"""Student Class"""
 
 
 class Student:
-    """Student."""
+    """Class Student that defines a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Student.
-
-        Args:
-            first_name (str): first_name
-            last_name (str): last_name
-            age (int): age
-        """
         self.first_name = first_name
         self.last_name = last_name
-        self.age = int(age)
+        self.age = age
 
-    def to_json(self):
-        """To_json."""
-        return self.__dict__
+    def to_json(self, attrs=None):
+        if (attrs is None):
+            return (self.__dict__)
+        dictionary = dict()
+        for element in attrs:
+            for key in self.__dict__:
+                if (element == key):
+                    dictionary[element] = self.__dict__[key]
+        return (dictionary)
