84bd5a2e661a7002829c3bb0cd53bf35bc5208d2
Magedsaif <magedsaif21@gmail.com>
2023-08-16 00:47:51 +0200

    t  15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,8 @@
 -- groups
-SELECT score, count(*) as NUM FROM second_table GROUP BY second_table
+SELECT score, COUNT(score) AS 'number'
+FROM
+    second_table
+GROUP BY
+    score
+ORDER BY number
+    DESC;
