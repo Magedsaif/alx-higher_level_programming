dd618d7f3738d9c88ac9cebae26ce65234a867d7
Magedsaif <magedsaif21@gmail.com>
2023-08-17 17:07:09 +0200

    t 103

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- 101-not_a_comedy.sql
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN (
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy")
+ORDER BY tv_shows.title ASC;
