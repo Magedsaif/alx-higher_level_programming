e41e99f4a8f85359b3eb21c4fa78d7379a341478
Magedsaif <magedsaif21@gmail.com>
2023-08-17 14:02:22 +0200

    t 7

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
--- -- create database and TABLE unique_id
+-- -- create database and TABLE states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- -- create database and TABLE cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    Foreign Key (state_id) REFERENCES states (id)
+);
