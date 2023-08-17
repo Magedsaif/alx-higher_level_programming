f8b3d11c2c4020c585e7e07c87007c8d76d34733
Magedsaif <magedsaif21@gmail.com>
2023-08-17 16:45:55 +0200

    t11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- 11-genre_id_all_shows.sql
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
