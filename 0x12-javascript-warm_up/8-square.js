b58a6186b456b0023d60938bcbb980fc642a5066
magedsaif <magedsaif21@gmail.com>
2023-09-11 21:34:25 +0200

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+if (isNaN(parseInt(process.argv[2], 10))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
+    let square = '';
+    for (let j = 0; j < parseInt(process.argv[2], 10); j++) {
+      square += 'x';
+    }
+    console.log(square);
+  }
+}
