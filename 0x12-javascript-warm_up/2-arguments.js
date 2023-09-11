304e6e2d1abc83aa3f9672dcf20db935a4d3d4db
magedsaif <magedsaif21@gmail.com>
2023-09-11 19:51:10 +0200

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2]) {
+if (!process.argv[2]) {
+  console.log('No argument');
+}
+if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 if (process.argv[2] && process.argv[3]) {
-    console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
-  } else {
-    console.log(process.argv[2] + ' ' + 'is' + ' ' + 'undefined');
-  }
-  
+  console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
+} else {
+  console.log(process.argv[2] + ' ' + 'is' + ' ' + 'undefined');
+}
