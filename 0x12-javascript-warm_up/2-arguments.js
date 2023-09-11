842bd98416119a619b128476c0c81e4da264b9d9
magedsaif <magedsaif21@gmail.com>
2023-09-11 19:34:10 +0200

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!process.argv[2]) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log('Arguments found');
+} else {
+  console.log('No argument');
 }
