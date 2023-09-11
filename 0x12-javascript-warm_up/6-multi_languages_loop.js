c897414247c2ecab31f7a48b7aea5b6b48f470a6
magedsaif <magedsaif21@gmail.com>
2023-09-11 20:51:16 +0200

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (i = 0; i < myarray.length; i++) {
+for (let i = 0; i < myarray.length; i++) {
   console.log(myarray[i]);
 }
