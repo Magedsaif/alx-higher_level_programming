652d51691a0840165ad8294447e9d662e24e439d
magedsaif <magedsaif21@gmail.com>
2023-09-17 13:29:30 +0200

    task 100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class State."""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class State."""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class."""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
