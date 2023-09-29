fbc6b7ff4fb378013a07445469cb94e65280ad64
magedsaif <magedsaif21@gmail.com>
2023-09-30 01:03:05 +0200

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Script that fetches https://intranet.hbtn.io/status."""
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(html.__class__))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
