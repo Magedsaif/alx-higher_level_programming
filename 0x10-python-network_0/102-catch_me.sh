e618dd94a6be92f245d8549ca55bb6fb7b69f43a
magedsaif <magedsaif21@gmail.com>
2023-09-29 23:16:11 +0200

    task 101 & 102

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#  a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -s -X PUT -L -H "Origin:School" 0.0.0.0:5000/catch_me_3
