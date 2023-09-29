87455c777209c7224166be28e3a90d9e3e3c5434
magedsaif <magedsaif21@gmail.com>
2023-09-29 20:54:45 +0200

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# a Bash script that takes in a URL, sends a request to that URL and displays the size of the body of the response
 curl -w "%{size_download}\n" -o /dev/null -s $1
