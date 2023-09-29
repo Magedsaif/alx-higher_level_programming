843096c9f069afdf366f32e7fa706040ecc81284
magedsaif <magedsaif21@gmail.com>
2023-09-30 01:18:49 +0200

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Script that fetches https://alx-intranet.hbtn.io/status."""
 
-import urllib.request
-
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+                                as response:
         contents_read = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(contents_read)))
