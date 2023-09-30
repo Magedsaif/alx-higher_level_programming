0a3f383d2c0369248e8b55b8d9bac1a6d8d1b1b5
magedsaif <magedsaif21@gmail.com>
2023-09-30 02:47:55 +0200

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers('X-Request-Id'))
+    print(response.headers.get('X-Request-Id'))
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email address,
+    sends a POST request to the passed URL with the email as a parameter,
+    and finally displays the body of the response.
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+    print(response.text)
