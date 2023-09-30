139e4e943ba914d17b93920636751062f5027d0a
magedsaif <magedsaif21@gmail.com>
2023-09-30 02:34:54 +0200

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 """Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print('Body response:')
     print('\t- type: {}'.format(type(response.text)))
