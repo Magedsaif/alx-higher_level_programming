23b74209b7fa45be22b1742bac061cd7783ee34c
magedsaif <magedsaif21@gmail.com>
2023-09-30 02:24:38 +0200

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    response = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
