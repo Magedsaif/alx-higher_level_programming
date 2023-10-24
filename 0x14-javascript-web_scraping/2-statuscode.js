67ad21c2b1ab0941080ebd740d9c3da18337567e
magedsaif <magedsaif21@gmail.com>
2023-10-24 12:25:20 +0200

    task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,6 @@
 // a script that display the status code of a GET request.
 // The first argument is the URL to request (GET)
 // The status code must be printed like this: code: <status code>
-// You must use the module request
-// You must use the method GET
-// You must use the module request
-// You must use the method GET
 
 const request = require('request');
 const url = process.argv[2];
---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that prints the title of a Star Wars movie where the episode number matches a given integer.
+// The first argument is the movie ID
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
