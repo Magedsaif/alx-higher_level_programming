64aaea034d6394546a1ae5cba475c08d64483fec
Maged Saif <magedsaif21@gmail.com>
2023-11-07 20:46:20 +0200

    task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to query the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    return 0
